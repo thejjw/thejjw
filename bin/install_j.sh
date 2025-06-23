e24a321f7061154435e7112b221ac90b17997dc2
thejjw <72130076+thejjw@users.noreply.github.com>
2025-06-23 04:05:32 +0000

    Add PATH export to .bashrc in install_j.sh

--- bin/install_j.sh
+++ bin/install_j.sh
@@ -9,3 +9,8 @@ fi
 
 PACKAGES="libjxl-tools tmux build-essential nodejs cmatrix fonts-noto-cjk neofetch curl parallel zstd xz-utils"
 sudo apt-get update && sudo apt-get install -y $PACKAGES
+
+if ! grep -q 'export PATH="$HOME/bin:$PATH"' "$HOME/.bashrc"; then
+  echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
+  echo "Added ~/bin to PATH in .bashrc"
+fi
