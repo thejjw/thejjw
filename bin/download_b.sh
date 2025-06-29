a108792b426e614f59be4b23a7d7e935335955ff
thejjw <72130076+thejjw@users.noreply.github.com>
2025-06-29 06:26:53 +0000

    chore: Update download_b.sh to sort out execution order

--- bin/download_b.sh
+++ bin/download_b.sh
@@ -8,9 +8,9 @@ cd thejjw
 git sparse-checkout init --cone
 git sparse-checkout set bin
 git checkout main
+chmod +x bin/*
 mkdir -pv ~/bin
-cp -rv bin/* ~/bin/
-chmod +x ~/bin/*
+cp -arv bin/* ~/bin/
 
 if ! grep -q 'export PATH="$HOME/bin:$PATH"' "$HOME/.bashrc"; then
   echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bashrc"
