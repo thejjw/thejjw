1153897225834e4530657ccc79705f6f202757b0
thejjw <72130076+thejjw@users.noreply.github.com>
2025-07-29 04:54:37 +0000

    chore: Add error handling to download_b.sh script

--- bin/download_b.sh
+++ bin/download_b.sh
@@ -3,6 +3,9 @@
 # clone only the bin/ directory
 # 2025.6 @thejjw
 
+# Exit on any error
+set -e
+
 git clone --filter=blob:none --no-checkout https://github.com/thejjw/thejjw.git
 cd thejjw
 git sparse-checkout init --cone
