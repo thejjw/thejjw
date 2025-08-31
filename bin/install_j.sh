07df55a5d1211f0df62d8d1f809e8366d526fcc3
thejjw <72130076+thejjw@users.noreply.github.com>
2025-08-31 15:43:05 +0900

    Add list of install packages

--- bin/install_j.sh
+++ bin/install_j.sh
@@ -7,5 +7,5 @@ if ! which apt >/dev/null; then
   exit 1
 fi
 
-PACKAGES="libjxl-tools tmux build-essential nodejs cmatrix fonts-noto-cjk neofetch curl parallel zstd xz-utils"
+PACKAGES="libjxl-tools tmux build-essential nodejs cmatrix fonts-noto-cjk neofetch curl parallel zstd xz-utils webp"
 sudo apt-get update && sudo apt-get install -y $PACKAGES
