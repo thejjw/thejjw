85b86091ff4b33d50d81423806ef4c1deac5076b
thejjw <72130076+thejjw@users.noreply.github.com>
2025-11-12 08:45:50 +0900

    Update package list and install Node.js via NVM
    Removed 'nodejs' from the package list and added NVM installation.

--- bin/install_j.sh
+++ bin/install_j.sh
@@ -7,5 +7,8 @@ if ! which apt >/dev/null; then
   exit 1
 fi
 
-PACKAGES="libjxl-tools tmux build-essential nodejs cmatrix fonts-noto-cjk neofetch curl parallel zstd xz-utils webp btop zram-tools"
+PACKAGES="libjxl-tools tmux build-essential cmatrix fonts-noto-cjk neofetch curl parallel zstd xz-utils webp btop zram-tools"
 sudo apt-get update && sudo apt-get install -y $PACKAGES
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+source ~/.nvm/nvm.sh && nvm install --lts
