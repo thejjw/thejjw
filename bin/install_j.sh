6ca16c478b04c8ba51fcd0698ee6ad42ee0efef3
thejjw <72130076+thejjw@users.noreply.github.com>
2026-03-27 10:45:27 +0900

    Upgrade NVM version in install_j.sh
    Update NVM installation script to version 0.40.4

--- bin/install_j.sh
+++ bin/install_j.sh
@@ -10,5 +10,6 @@ fi
 PACKAGES="libjxl-tools tmux build-essential cmatrix fonts-noto-cjk neofetch curl parallel zstd xz-utils webp btop zram-tools"
 sudo apt-get update && sudo apt-get install -y $PACKAGES
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+NVM_VERSION="v0.40.4"
+curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh" | bash
 source ~/.nvm/nvm.sh && nvm install --lts
